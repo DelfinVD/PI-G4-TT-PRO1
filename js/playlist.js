140f67834d0f1f346bd59cbc31024cc97e0e4bae
DelfinVD <dvanditmar@udesa.edu.ar>
2023-06-16 17:57:06 -0300

    Trabajo de playlist y search Results

--- js/playlist.js
+++ js/playlist.js
@@ -23,3 +23,46 @@ form.addEventListener('submit', function(e){
         this.sumbit();
     }
 })
+
+
+
+let recuperoStorage = localStorage.getItem('cancionesFavoritas');
+
+let cancionesFavoritas = JSON.parse(recuperoStorage)
+
+let ulLista = document.querySelector('.cancionesFavoritasList');
+
+let miLista = '';
+
+ 
+if (cancionesFavoritas == null || cancionesFavoritas.length == 0) {
+    ulLista.innerHTML = '<p>No hay canciones en tu playlist</p>'
+} else {
+    
+    for (let i = 0; i < cancionesFavoritas.length; i++) {
+
+        let url = `https://api.allorigins.win/raw?url=https://api.deezer.com/track/${cancionesFavoritas[i]}`;
+
+        fetch(url)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            
+                    miLista += `<li class= "liFavoritos">
+                                     <img class='imgCancionFavorita' src="${data.album.cover}" alt="imagenCanciónFavorita">
+                                     <a class='playlistDetalleSong' href="./detalleCancion.html?id=${data.id}">
+                                     <h3 class='nombreCancionFavorita' href="./detalleCancion.html?id=${data.id}">${data.title}</h3>
+                                     <p class="nombreArtistaCanciónFavorita">${data.artist.name}</p>
+                                    </a>
+                                    </li>`
+
+             ulLista.innerHTML = miLista;
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+        
+    }
+}
